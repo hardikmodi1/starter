f940d16a826041d2eb4cb01fb9b6b5850f125f6a
hardiked <hmodi97122@yahoo.in>
2019-10-26 13:02:48 +0530

    setup tslint and prettier

--- packages/typegraphql_boilerplate/src/modules/user/ConfirmUser.ts
+++ packages/typegraphql_boilerplate/src/modules/user/ConfirmUser.ts
@@ -1,18 +1,18 @@
-import { Arg, Mutation, Resolver } from "type-graphql";
-import { User } from "../../entity/User";
-import { redis } from "../../redis";
-import { confirmUserPrefix } from "../constants/redisPrefixes";
+import { Arg, Mutation, Resolver } from 'type-graphql'
+import { User } from '../../entity/User'
+import { redis } from '../../redis'
+import { confirmUserPrefix } from '../constants/redisPrefixes'
 
 @Resolver()
 export class ConfirmUserResolver {
-	@Mutation(() => Boolean)
-	async confirmUser(@Arg("token") token: string): Promise<boolean> {
-		const userId = await redis.get(confirmUserPrefix + token);
-		if (!userId) {
-			return false;
-		}
-		await User.update({ id: parseInt(userId, 10) }, { confirmed: true });
-		await redis.del(confirmUserPrefix + token);
-		return true;
-	}
+  @Mutation(() => Boolean)
+  async confirmUser(@Arg('token') token: string): Promise<boolean> {
+    const userId = await redis.get(confirmUserPrefix + token)
+    if (!userId) {
+      return false
+    }
+    await User.update({ id: userId }, { confirmed: true })
+    await redis.del(confirmUserPrefix + token)
+    return true
+  }
 }
--- packages/typegraphql_boilerplate/src/types/Context.ts
+++ packages/typegraphql_boilerplate/src/types/Context.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express'
 
 export interface Context {
-	req: Request;
-	res: Response;
+  req: Request
+  res: Response
 }
