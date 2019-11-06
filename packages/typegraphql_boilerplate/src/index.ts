93bb72bb97cdc5ed0eb08e179f65139d38a6f598
hardikmodi1 <hmodi2457@gmail.com>
2019-11-06 16:04:52 +0530

    make docker to work

--- packages/typegraphql_boilerplate/src/index.ts
+++ packages/typegraphql_boilerplate/src/index.ts
@@ -57,7 +57,7 @@ const main = async () => {
       saveUninitialized: false,
       cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: false,
         maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
       },
     })
